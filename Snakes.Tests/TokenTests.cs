ffe0b813d064878616a6648e1e2dd40c691cde42
josepo <jlpolo@gmail.com>
2021-07-10 18:06:15 +0200

    Token moves plus refactor

--- Snakes.Tests/TokenTests.cs
+++ Snakes.Tests/TokenTests.cs
@@ -2,14 +2,22 @@ using Xunit;
 
 namespace Snakes.Tests
 {
-    public class TokenTests
-    {
-        [Fact]
-        public void NewTokenInStartingPosition()
-        {
-           Token token = new Token(0);
+   public class TokenTests
+   {
+      [Fact]
+      public void NewTokenInStartingPosition()
+      {
+         Token token = new Token();
 
-           Assert.Equal(1, token.Position);
-        }
-    }
+         Assert.Equal(1, token.Position);
+      }
+
+      [Fact]
+      public void TokenMoves()
+      {
+         Token token = new Token().Move(3);
+
+         Assert.Equal(4, token.Position);
+      }
+   }
 }
--- Snakes/Token.cs
+++ Snakes/Token.cs
@@ -2,13 +2,18 @@ namespace Snakes
 {
    public class Token
    {
-      public int Id { get; private set; }
       public int Position { get; private set; }
 
-      public Token(int id)
+      public Token() : this(1) {}
+
+      private Token(int position)
       {
-         Id = id;
-         Position = 1;
+         Position = position;
+      }
+
+      public Token Move(int moves)
+      {
+         return new Token(Position + moves);
       }
    }
 }
